355123735b1930a952bb966a1f7be929bce1ea0d
K0ala <92472219+K0ala-py@users.noreply.github.com>
2023-06-28 22:56:44 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+password = input('Enter password : ')
+if password == '1674152845266890757':
+  print('Scanning... ')
+  time.sleep(3)
+  print('Done!')
+  time.sleep(1)
+  print('The Answer is : S3cr3tPassword000')
